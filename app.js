9ad0a87f218b4bebc2adc8adc4525423b5f1e1bc
Dygy <Yukimurachris@gmail.com>
2019-06-21 15:52:51 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const WebSocket= require('ws');
+const  server = new WebSocket.Server({port:3000});
+let index=0;
+let AWS = require('aws-sdk');
+AWS.config.update({ accessKeyId: '...', secretAccessKey: '...' });
+app.set("view", path.join(__dirname, "./view"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./view/selfie'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/view/upload/index.html');
+});
+app.get('/selfie', (req, res) => {
+    res.sendFile(__dirname + '/view/selfie/selfie.html');
+});
+server.on('connection',ws =>{
+    ws.on('message',message=>{
+        let base64Data = message['URL'].replace('data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=image.png;base64,',"")
+        fs.writeFile(path.resolve(__dirname, './uploads/image '+index+'.png'), base64Data, 'base64', function(err) {
+            if (err) throw err;
+        });
+        s3fun(base64Data,'image '+index+'.png',message['type']);
+        index++;
+        while (!s3Check('image '+index+'.png',message['type'])){}
+        //send photo back to client
+        server.clients.forEach(clients => {
+            if (clients.readyState=== WebSocket.OPEN){
+
+            }});});});
+
+const upload = multer({
+    dest: "./uploads"
+    // you might also want to set some limits: https://github.com/expressjs/multer#limits
+});
+
+app.post(
+    "/upload",
+    upload.single("file" /* name attribute of <file> element in your form */),
+    (req, res) => {
+        const tempPath = req.file.path;
+        const targetPath = path.join(__dirname, "./uploads/image.png");
+
+        if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+            fs.rename(tempPath, targetPath, err => {
+                if (err) return handleError(err, res);
+                res
+                    .status(200)
+                    .contentType("text/plain")
+                    .end("File uploaded!");
+            });
+        } else {
+            fs.unlink(tempPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(403)
+                    .contentType("text/plain")
+                    .end("Only .png files are allowed!");
+            });
+        }
+    }
+);
+
+
+let s3Check = async (bucketName,fileName) => {
+    let params = {
+        Bucket: bucketName,
+        Key: fileName
+    };
+    try {
+        const headCode = await s3.headObject(params).promise();
+        const signedUrl = await s3.getSignedUrl('getObject', params).promise();
+        // Do something with signedUrl
+        return true
+    } catch (headErr) {
+        if (headErr.code === 'NotFound') {
+            return false
+            // Handle no object on cloud here
+        }
+    }
+};
+let s3fun = (data,index,bucketName,fileName)=> {
+        let base64data = Buffer.from(data, 'binary');
+        let s3 = new AWS.S3();
+        s3.putObject( {
+        Bucket: bucketName,
+        Key: fileName,
+        timeout: 6000000,
+        Body: base64data,
+        ACL: 'public-read',
+        ContentType: 'binary'
+        }, ( error, data ) => {
+            if( error ) console.log( error );
+            console.log(data)
+        });
+};
+app.get("/image.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "./uploads/image.png"));
+});
+
+app.listen(process.env.PORT || 5000,'0.0.0.0', () => console.log('Example app listening on port 5000!'));
---
+++ view/selfie/app.js
@@ -0,0 +1,54 @@
+const ws = new WebSocket('ws://localhost:3000');
+// Set constraints for the video stream
+
+let constraints = {video: {facingMode: "user"}, audio: false};
+let track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/png");
+    cameraOutput.classList.add("taken");
+    let url = cameraOutput.src.replace(/^data:image\/[^;]+/, 'data:application/octet-stream');
+    dlCanvas(url)
+    // track.stop();
+};
+function dlCanvas(url) {
+    url = url.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=image.png');
+    ws.send({'URL':url,type:'selfie'});
+    document.getElementById('submit--trigger').href=url
+}
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+
+// Install ServiceWorker
+if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+        console.log('CLIENT: service worker registration complete.');
+    }, function() {
+        console.log('CLIENT: service worker registration failure.');
+    });
+} else {
+    console.log('CLIENT: service worker is not supported.');
+}
